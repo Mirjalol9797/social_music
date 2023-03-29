4c3ce3cb6428a5ba45c62faeca2e96e6c060a7d5
Mirxomitov Mirjalol <miramirxomitov97@gmail.com>
2023-03-29 06:30:53 +0500

    added cropped image and some change in edit profile

--- src/views/account/EditProfile.vue
+++ src/views/account/EditProfile.vue
@@ -2,14 +2,14 @@
   <div id="EditProfile" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
     <div class="text-gray-900 text-xl">Edit Profile</div>
     <div class="bg-green-500 w-full h-1"></div>
-    <!--
+
     <CropperModal
       v-if="showModal"
       :minAspectRatioProp="{ width: 8, height: 8 }"
       :maxAspectRatioProp="{ width: 8, height: 8 }"
       @croppedImageData="setCroppedImageData"
       @showModal="showModal = false"
-    /> -->
+    />
 
     <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full md:w-1/2 px-3">
@@ -48,16 +48,16 @@
         <DisplayCropperButton
           label="Profile Image"
           btnText="Update Profile Image"
-          @showModal="showModal"
+          @showModal="showModal = true"
         />
       </div>
     </div>
 
-    <!-- <div class="flex flex-wrap mt-4 mb-6">
+    <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full md:w-1/2 px-3">
         <CroppedImage label="Cropped Image" :image="image" />
       </div>
-    </div> -->
+    </div>
 
     <div class="flex flex-wrap mt-4 mb-6">
       <div class="w-full px-3">
@@ -65,15 +65,16 @@
           label="Description"
           placeholder="Please enter some information here!!!"
           v-model:description="description"
+          error="errors.last_name ? errors.last_name[0] : ''"
         />
       </div>
     </div>
 
-    <!-- <div class="flex flex-wrap mt-8 mb-6">
+    <div class="flex flex-wrap mt-8 mb-6">
       <div class="w-full px-3">
-        <SubmitFormButton btnText="Update Profile" @click="updateUser" />
+        <SubmitFormButton btnText="Update Profile" @submit="updateUser" />
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -83,12 +84,24 @@ import { ref } from "vue";
 import TextInput from "@/components/global/TextInput.vue";
 import DisplayCropperButton from "@/components/global/DisplayCropperButton.vue";
 import TextArea from "@/components/global/TextArea.vue";
+import SubmitFormButton from "@/components/global/SubmitFormButton.vue";
+import CropperModal from "@/components/global/CropperModal.vue";
+import CroppedImage from "@/components/global/CroppedImage.vue";
 
+const showModal = ref(false);
 const firstName = ref(null);
 const lastName = ref(null);
 const location = ref(null);
+const description = ref(null);
+// const ImageData = ref(null);
+const image = ref(null);
 
-function showModal(status) {
+const setCroppedImageData = (data) => {
+  // ImageData = data;
+  image.value = data.imageUrl;
+};
+
+function updateUser(status) {
   console.log(status);
 }
 </script>
